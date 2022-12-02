a208b1cde579ff06756b023c4a451d0b84101587
Manikandan Sethuraj <mani@bnsellit.com>
2022-12-02 00:34:09 -0500

    Commit 3
    Signed-off-by: Manikandan Sethuraj <mani@bnsellit.com>
    
    Commit 2
    
    Signed-off-by: Manikandan Sethuraj <mani@bnsellit.com>

--- curd/src/main/java/com/manny/curd/MainActivity.kt
+++ curd/src/main/java/com/manny/curd/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Commit 1
+        // Commit 2
+        // Commit 3
+
     }
 }
